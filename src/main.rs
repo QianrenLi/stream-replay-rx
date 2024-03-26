e3f82e0257eea253572f09186698767357945651
QianrenLi <908559578@qq.com>
2024-03-26 13:19:07 +0800

    update: rust based udp rx

--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,131 @@
-mod rx;
-use std::collections::HashMap;
-use std::net::{UdpSocket, SocketAddr};
-use std::thread;
+
+use std::net::UdpSocket;
 use std::sync::{Arc, Mutex};
-use std::time::{Duration, SystemTime};
-use crate::rx::Args;
+use clap::Parser;
+use std::io::ErrorKind;
+
+
+
 const PONG_PORT_INC: u16 = 1024;
 
-static mut RECEIVED_LENGTH: usize = 0;
-static mut RECEIVED_RECORD: HashMap<u32, (f64, f64)> = HashMap::new();
-static mut INIT_TIME: Option<SystemTime> = None;
-
-use clap::Parser;
-#[derive(Parser, Debug)]
-// #[clap(version = "1.0", author = "Yuki Ueda")]
-#[clap(version = "1.0")]
+#[derive(Parser)]
 struct Args {
-    #[clap(short = 'p', long = "port")]
     port: u16,
-    #[clap(short = 'd', long = "duration")]
-    duration: u16,
-    #[clap(long = "calc-jitter")]
-    calc_jitter: bool,
-    #[clap(long = "calc-rtt")]
-    calc_rtt: bool,
-    #[clap(short = 't', long = "tos")]
-    tos: u8,
+    duration: u32,
+    calc_rtt : bool
 }
 
+struct RecvData{
+    seq_offset: Vec<u32>,
+    data_len: u32,
+    rx_start_time: u64,
+}
 
+impl RecvData{
+    fn new() -> Self{
+        Self{
+            seq_offset: vec![0; 1000000],
+            data_len: 0,
+            rx_start_time: 0,
+        }
+    }
+}
 
 fn main() {
     let args = Args::parse();
-    let sock = UdpSocket::bind(("0.0.0.0", args.port)).unwrap();
-    if args.calc_rtt {
-        let pong_port = args.port + PONG_PORT_INC;
-        let pong_sock = UdpSocket::bind(("0.0.0.0:{}", pong_port)).unwrap();
-    } 
+    let recv_data = Arc::new(Mutex::new(RecvData::new()));
+    let recv_data_thread = Arc::clone(&recv_data);
 
-    let trigger = Arc::new(Mutex::new(()));
+    // Extract duration from args
+    let duration = args.duration.clone();
+    
+    let lock = Arc::new(Mutex::new(false));
+    let lock_clone = Arc::clone(&lock);
+    
+    std::thread::spawn(move || {
+        recv_thread(args, recv_data_thread, lock_clone);
+    });
 
-    println!("waiting ...");
-    {
-        let trigger = trigger.clone();
-        let sock = sock.try_clone().unwrap();
-        let pong_port = pong_port.clone();
-        let pong_sock = pong_sock.try_clone().unwrap();
-        thread::spawn(move || recv_thread(args, sock, pong_port, pong_sock, trigger));
+    
+    while !*lock.lock().unwrap() {
+        std::thread::sleep(std::time::Duration::from_nanos(100_000) );
     }
 
-    let mut buffer = [0u8; 10240];
-    sock.recv_from(&mut buffer).unwrap();
-    if args.calc_jitter {
-        let (timestamp, init_seq, _, _, _) = extract(&buffer);
-        RECEIVED_RECORD.insert(init_seq, (timestamp, SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs_f64()));
-    }
-    unsafe { INIT_TIME = Some(SystemTime::now()); }
+    // Sleep for the duration
+    std::thread::sleep(std::time::Duration::from_secs(duration as u64));
+    let data_len = recv_data.lock().unwrap().data_len;
+    println!("Received Bytes: {:.3} MB", data_len as f64/ 1024.0 / 1024.0);
+    let rx_duration = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs() - recv_data.lock().unwrap().rx_start_time;
+    println!("Average Throughput: {:.3} Mbps", (data_len  * 8 ) as f64 / rx_duration as f64 / 1e6);
+}
 
-    if let Some(duration) = args.duration {
-        thread::sleep(Duration::from_secs(duration as u64));
-    } else if let Some(length) = args.length {
-        while unsafe { RECEIVED_LENGTH } < length as usize {
-            thread::sleep(Duration::from_millis(10));
+fn recv_thread(args: Args, recv_params: Arc<Mutex<RecvData>>, lock: Arc<Mutex<bool>>){
+    let addr = format!("0.0.0.0:{}", args.port);    
+    let socket = UdpSocket::bind(&addr).unwrap();
+    socket.set_nonblocking(true).unwrap();
+
+    let addr = format!("0.0.0.0:0");
+    let pong_socket = UdpSocket::bind(&addr).unwrap();
+    pong_socket.set_nonblocking(true).unwrap();
+    
+    println!("Waiting ...");
+
+    let mut buffer = [0; 10240];
+    loop {
+        if let Ok((_len, _src_addr)) = socket.recv_from(&mut buffer) {
+            *lock.lock().unwrap() = true;
+            
+            let mut data = recv_params.lock().unwrap();
+            data.data_len += _len as u32;
+
+            data.rx_start_time = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
+
+            let seq = u32::from_le_bytes(buffer[0..4].try_into().unwrap());
+            let _offset = u32::from_le_bytes(buffer[4..6].try_into().unwrap());
+
+            while data.seq_offset.len() <= seq as usize {
+                data.seq_offset.push(0);
+            }
+            data.seq_offset[seq as usize] += 1;
+            println!("Start");
+            break;
         }
-    } else {
-        panic!("Either [--duration] or [--length] should be specified.");
+        else {
+            std::thread::sleep(std::time::Duration::from_nanos(100_000) );
+        }
     }
 
-    // Print completion time or received bytes and average throughput
-    if let Some(length) = args.length {
-        let duration = SystemTime::now().duration_since(unsafe { INIT_TIME.unwrap() }).unwrap();
-        println!("Completion Time: {:.3} s", duration.as_secs_f64());
-    } else {
-        let duration = args.duration.unwrap();
-        println!("Received Bytes: {:.3} MB", unsafe { RECEIVED_LENGTH as f64 / 1024.0 / 1024.0 });
-    }
+    loop {
+        let mut buffer = [0; 2048];
+        if let Ok((_len, src_addr)) = socket.recv_from(&mut buffer) {
+            let mut data = recv_params.lock().unwrap();
+            data.data_len += _len as u32;
+            if !args.calc_rtt {
+                continue;
+            }
 
-    // Print average jitter if --calc-jitter is specified
-    if args.calc_jitter {
-        let average_delay_ms
-        let average_delay_ms: Vec<f64> = RECEIVED_RECORD.values()
-            .filter_map(|record| {
-                if let Some(timestamp) = record.0 {
-                    Some((SystemTime::now().duration_since(timestamp).unwrap().as_secs_f64()) * 1000.0)
-                } else {
-                    None
+            let seq = u32::from_le_bytes(buffer[0..4].try_into().unwrap());
+            let _offset = u32::from_le_bytes(buffer[4..6].try_into().unwrap());
+            let num = u32::from_le_bytes(buffer[6..8].try_into().unwrap());
+
+            while data.seq_offset.len() <= seq as usize {
+                data.seq_offset.push(0);
+            }
+            data.seq_offset[seq as usize] += 1;
+            
+            if data.seq_offset[seq as usize] == num {
+                let modified_addr = format!("{}:{}", src_addr.ip(), args.port + PONG_PORT_INC);
+                match pong_socket.send_to(&mut buffer[.._len], &modified_addr) {
+                    Ok(_) => {},
+                    Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
+                        println!("Send operation would block, retrying later...");
+                    }
+                    Err(e) => {
+                        eprintln!("Error sending data: {}", e);
+                    }
                 }
-            })
-            .collect();
+            }
 
-        let average_jitter_ms = average_delay_ms.windows(2)
-            .map(|pair| pair[1] - pair[0])
-            .sum::<f64>() / (average_delay_ms.len() - 1) as f64;
-
-        println!("Average Jitter: {:.6} ms", average_jitter_ms);
+        }
     }
 }
